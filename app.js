ea9cbe2b473be34f5b2138534adb87c6b86847f6
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2023-09-27 14:57:20 +0300

    sd

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.connect(DATA_MOVIES, {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:3000', 'https://api.bubaleha.nomoredomains.rocks'], credentials: true, maxAge: 30 }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://api.bubaleha.nomoredomains.rocks'] }));
 app.use(requestLogger);
 app.use(rateLimiter);
 app.use(hemlet());
