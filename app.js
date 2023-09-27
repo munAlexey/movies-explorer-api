fa97a129e9d3570be6c54cc9ebd23492cbaf4d75
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2023-09-27 15:06:53 +0300

    sds;

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
-const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { CENTRAL_ERROR_HANDLER } = require('./errors/central-error-handler');
 const Router = require('./routes/index');
@@ -25,14 +24,8 @@ mongoose.connect(DATA_MOVIES, {
   res.status(500).send({ message: 'Unauthorized' });
 });
 
-const corsOptions = {
-  origin: ['https://api.bubaleha.nomoredomains.rocks', 'http://localhost:3000'],
-  optionsSuccessStatus: 200,
-};
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 app.use(requestLogger);
 app.use(rateLimiter);
 app.use(hemlet());
