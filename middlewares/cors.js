ba8f3392c47f7083257722a0c65518d5237e25af
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2023-09-27 12:15:32 +0300

    s

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,17 @@
+const allowedCors = [
+  'https://api.bubaleha.nomoredomains.rocks',
+  'https://bubaleha.nomoredomains.monster',
+  'https://localhost:3000',
+];
+
 const cors = (req, res, next) => {
+  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Origin', '*');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
