3c03e620dec76e68d58d273f1e750acd199c517a
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2023-10-04 10:56:21 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,6 +11,7 @@ const cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   }
 
   if (method === 'OPTIONS') {
